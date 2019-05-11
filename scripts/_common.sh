93c0b8eaf5235f3ba7dea097fcae7ab056ba2993
Баглий Антон Павлович <bagliy_ap@studzone.local>
2019-05-11 23:12:04 +0300

    ADD: proper nginx config, app setup

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -11,3 +11,35 @@ ynh_delete_file_checksum () {
 	local checksum_setting_name=checksum_${1//[\/ ]/_}	# Replace all '/' and ' ' by '_'
 	ynh_app_setting_delete $app $checksum_setting_name
 }
+
+#
+# Composer helpers
+#
+
+# Execute a composer command from a given directory
+# usage: composer_exec WORKDIR COMMAND [ARG ...]
+exec_composer() {
+  local WORKDIR=$1
+  shift 1
+
+  COMPOSER_HOME="${WORKDIR}/.composer" \
+    sudo php "${WORKDIR}/composer.phar" $@ \
+      -d "${WORKDIR}"  --no-interaction
+}
+
+# Install and initialize Composer in the given directory
+# usage: init_composer DESTDIR [AS_USER]
+init_composer() {
+  local DESTDIR=$1
+  local AS_USER=${2:-admin}
+
+  # install composer
+  curl -sS https://getcomposer.org/installer \
+    | COMPOSER_HOME="${DESTDIR}/.composer" \
+        sudo php -- --quiet --install-dir="$DESTDIR" \
+    || ynh_die "Unable to install Composer"
+
+  # update dependencies to create composer.lock
+  exec_composer "$DESTDIR" install  --no-dev --prefer-source \
+    || ynh_die "Unable to update snipe-it core dependencies"
+}
